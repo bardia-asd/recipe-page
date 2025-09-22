7fa167b4a3a47ec6efce60ff707e6e551fb2832e
bardia-asd <brdyaasdy5@gmail.com>
2025-09-22 09:13:18 +0330

    chore: install Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            
+        </>
+    );
 }
 
-export default App
+export default App;
